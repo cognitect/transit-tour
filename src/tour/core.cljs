254c64a5ba0ca41265c15f638a4b5e92ac1c756c
David Nolen <dnolen@cognitect.com>
2014-07-08 14:45:52 -0400

    acceptable eval for now

--- src/tour/core.cljs
+++ src/tour/core.cljs
@@ -4,27 +4,24 @@
 
 (enable-console-print!)
 
-;; repl.eval = function (code) {
-;;     try {
-;;         if (isExpression(code)) {
-;;             geval("__expression__ = " + code);
-;;             express(__expression__);
-;;         } else geval(code);
-;;     } catch (error) {
-;;         repl.print(error, "error");
-;;     }
-;; };
-
 ;; var myCodeMirror = CodeMirror(function(elt) {
 ;;   myTextArea.parentNode.replaceChild(elt, myTextArea);
 ;; }, {value: myTextArea.value});
 
+(def textareas (atom {}))
+
 (doseq [textarea (d/nodes (css/sel "textarea"))]
-  #_(js/CodeMirror.fromTextArea textarea
-    #js {:mode "javascript"
-         ;:theme "eclipse"
-         })
-  (js/configREPL
-    (js/CodeMirrorREPL. textarea
-      #js {:mode "javascript"
-           :theme "eclipse"})))
+  (let [id (.-id textarea)]
+    (swap! textareas assoc id
+      (js/CodeMirror.fromTextArea textarea
+        #js {:mode "javascript"}))))
+
+(set! (.-evaluate js/window.document)
+  (fn [id]
+    (let [cm   (get @textareas id)
+          re   (js/RegExp. "\n\\/\\/.*" "g")
+          orig (.replace (.getValue cm) re "")
+          ret  (js/window.eval orig)]
+      (if (undefined? ret)
+        (.setValue cm (str orig "\n// undefined"))
+        (.setValue cm (str orig "\n// " ret))))))
