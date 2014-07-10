69e33309fc20b60cb5cc9a1226392cef672a75f4
David Nolen <dnolen@cognitect.com>
2014-07-10 16:28:29 -0400

    add CodeMirror externs

---
+++ src/codemirror_externs.js
@@ -0,0 +1,7 @@
+/**
+ * @constructor
+ */
+var CodeMirror = function() {};
+CodeMirror.fromTextArea = function(el, options) {};
+CodeMirror.getValue = function() {};
+CodeMirror.setValue = function() {};
